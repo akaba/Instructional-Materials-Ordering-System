a74c3d5fc3ecfbf5dfc67e0fec1d3a8f76f43646
akaba <alikaba@ggmail.com>
2022-05-09 09:46:01 -0500

    updated version

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InstructionalMaterials.Startup))]
+namespace InstructionalMaterials
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
